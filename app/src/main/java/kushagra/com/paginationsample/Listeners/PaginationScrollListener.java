e62505794b5ff4caa9a2c6c33d34738e410dbad4
kushagra <kushagrasaxena03@gmail.com>
2018-03-18 16:52:34 +0530

    Initial commit

---
+++ app/src/main/java/kushagra/com/paginationsample/Listeners/PaginationScrollListener.java
@@ -0,0 +1,49 @@
+package kushagra.com.paginationsample.Listeners;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Kushagra Saxena on 17/03/2018.
+ */
+
+public abstract class PaginationScrollListener extends RecyclerView.OnScrollListener {
+
+    LinearLayoutManager layoutManager;
+
+    /**
+     * Supporting only LinearLayoutManager for now.
+     *
+     * @param layoutManager
+     */
+    public PaginationScrollListener(LinearLayoutManager layoutManager) {
+        this.layoutManager = layoutManager;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        int visibleItemCount = layoutManager.getChildCount();
+        int totalItemCount = layoutManager.getItemCount();
+        int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
+
+        if (!isLoading() && !isLastPage()) {
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
+                    && firstVisibleItemPosition >= 0
+                    && totalItemCount >= getTotalPageCount()) {
+                loadMoreItems();
+            }
+        }
+
+    }
+
+    protected abstract void loadMoreItems();
+
+    public abstract int getTotalPageCount();
+
+    public abstract boolean isLastPage();
+
+    public abstract boolean isLoading();
+
+}
